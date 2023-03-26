d23f5277848b1ed33ac3748d7a4c130bd919cb00
mln-grg <mgeorge00747@gmail.com>
2023-03-26 18:14:32 +0530

    Finalized board generator prototype

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject tilePrefab;
     [SerializeField] private Vector2 boardSize;
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private GameObject BoardPrefab;
     [Range(0,1)]
     [SerializeField] private float tileOffset;
+    [Range(-0.05f,-0.1f)]
     [SerializeField] private float tileHolderHeight;
-    
+
+    [SerializeField] private Material Black;
+    [SerializeField] private Material White;
     private void Start()
     {
         GenerateBoard();
@@ -23,10 +27,16 @@ public class BoardGenerator : MonoBehaviour
                 GameObject tile = GameObject.Instantiate(tilePrefab, transform);
                 tile.transform.position = new Vector3(-boardSize.x/2 + i + 0.5f,0,-boardSize.y / 2 +j + 0.5f);
                 tile.transform.localScale = Vector3.one*(1 - tileOffset);
+
+                tile.GetComponent<Renderer>().material = White;
             }
         }
 
-        GameObject tileHolder = GameObject.Instantiate(tilePrefab, transform);
-        tileHolder.transform.localScale=new Vector3(boardSize.x,0,boardSize.y);
+        GameObject tileHolder = GameObject.Instantiate(BoardPrefab, transform);
+        tileHolder.name = "TileHolder";
+        tileHolder.transform.localScale=new Vector3(boardSize.x,boardSize.y,1);
+        tileHolder.transform.position = new Vector3(0, tileHolderHeight, 0);
+
+        tileHolder.GetComponent<Renderer>().material = Black;
     }
 }
